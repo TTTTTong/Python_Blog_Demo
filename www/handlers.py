5dba9ac870e083ec2e0a6860ddf1b188e727e909
Tong <t15529208705@gmail.com>
2017-10-28 00:30:06 +0800

    html

--- www/handlers.py
+++ www/handlers.py
@@ -1,11 +1,18 @@
+import time
+
 from www.coroweb import get
-from www.models import User
+from www.models import User, Blog
 
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
+    summary = 'Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summar=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summar=summary, created_at=time.time()-7200),
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blog.html',
+        'blogs': blogs
     }
